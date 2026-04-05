0ddf15da0dc611523f4dd95a3a40ccf50458b64c
Muhammad Agus Saputra <muhgussaputra17@gmail.com>
2026-04-06 02:57:16 +0800

    fix(database): use PrismaPg adapter to resolve password parsing issue

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,9 +1,8 @@
+import 'dotenv/config';
 import { PrismaClient } from '../generated/prisma/client.ts';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
