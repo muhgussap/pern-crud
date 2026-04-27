14266a382535a9a854771ed687ae121e22dee239
Muhammad Agus Saputra <muhgussaputra17@gmail.com>
2026-04-28 00:37:24 +0800

    chore(frontend): inisialisasi konfigurasi prettier dan react entry point
    Menambahkan .prettierrc, .prettierignore, mengatur package.json, serta menerapkan format pada App.jsx dan main.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import { Routes, Route } from "react-router";
-import MainLayout from "./layouts/MainLayout";
-import TaskList from "./pages/TaskList";
-import TaskForm from "./pages/TaskForm";
+import { Routes, Route } from 'react-router';
+import MainLayout from './layouts/MainLayout';
+import TaskList from './pages/TaskList';
+import TaskForm from './pages/TaskForm';
 
 function App() {
-  return (
-    <Routes>
-      {/* MainLayout bertindak sebagai pembungkus utama */}
-      <Route element={<MainLayout />}>
-        {/* Rute-rute anak yang akan masuk ke dalam <Outlet /> di MainLayout */}
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add" element={<TaskForm />} />
-        <Route path="/edit/:id" element={<TaskForm />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            {/* MainLayout bertindak sebagai pembungkus utama */}
+            <Route element={<MainLayout />}>
+                {/* Rute-rute anak yang akan masuk ke dalam <Outlet /> di MainLayout */}
+                <Route path="/" element={<TaskList />} />
+                <Route path="/add" element={<TaskForm />} />
+                <Route path="/edit/:id" element={<TaskForm />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
